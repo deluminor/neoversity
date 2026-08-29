26a1d5f5e28bee62f2272d011ff41263f9ef0473
Erik K. <erickopcha@gmail.com>
2026-08-29 19:14:32 +0200

    feat: p. modal

---
+++ react/08-zustand/app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Modal from "@/components/Modal/Modal";
+
+export default function NotePreviewClient({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  const handleClose = () => {
+    router.back();
+  };
+
+  return <Modal onClose={handleClose}>{children}</Modal>;
+}
